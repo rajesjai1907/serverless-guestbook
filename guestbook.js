d4e31571c537cc54a6b74eea50095fd69c9bed21
rajesjai1907 <85188405+rajesjai1907@users.noreply.github.com>
2021-08-07 23:39:44 +0530

    Added API route/url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://faca8f8e.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
